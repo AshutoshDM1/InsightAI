cf1eb689f876c72803eb5d73584ecf3fed0370a4
AshutoshDM1 <downlodemaster1@gmail.com>
2024-07-13 21:00:57 +0530

    Add Vercel

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
-
-
   return (
     <>
-      
+    <Analytics/>
+     <h1>Hello World</h1> 
+     
     </>
   )
 }
